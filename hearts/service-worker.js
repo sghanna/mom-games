7e24a0bc9947580b84a32d72e2ba0c4f76fa85b8
sghanna <sghanna@gmail.com>
2026-09-17 22:25:57 -0700

    Bring back selection pulse, limited to 2 cycles

--- hearts/service-worker.js
+++ hearts/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "hearts-v10";
+const CACHE_NAME = "hearts-v11";
 const APP_FILES = [
   "./",
   "./index.html",
