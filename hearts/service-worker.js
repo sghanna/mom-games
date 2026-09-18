58768e9347b6c6625198a01a444e1c5f0af39f20
sghanna <sghanna@gmail.com>
2026-09-18 08:54:23 -0700

    Fix card alignment: revert to a single centered rank+suit group instead of two independent halves, which left a large dead gap in the middle (confirmed via iPhone 13 mini screenshot)

--- hearts/service-worker.js
+++ hearts/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "hearts-v14";
+const CACHE_NAME = "hearts-v15";
 const APP_FILES = [
   "./",
   "./index.html",
