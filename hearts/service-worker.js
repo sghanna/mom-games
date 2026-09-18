510e02539dffcc774ad7ec685c91a6b44e8f5d1c
sghanna <sghanna@gmail.com>
2026-09-18 08:33:33 -0700

    Guarantee at least 5px whitespace above rank and below suit on card faces

--- hearts/service-worker.js
+++ hearts/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "hearts-v12";
+const CACHE_NAME = "hearts-v13";
 const APP_FILES = [
   "./",
   "./index.html",
