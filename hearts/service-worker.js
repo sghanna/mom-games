06478006eac5e63275d825c3d81fab4dc01689cb
sghanna <sghanna@gmail.com>
2026-09-17 22:41:52 -0700

    Set selected-card animation to none (static magenta ring, no pulse)

--- hearts/service-worker.js
+++ hearts/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "hearts-v11";
+const CACHE_NAME = "hearts-v12";
 const APP_FILES = [
   "./",
   "./index.html",
