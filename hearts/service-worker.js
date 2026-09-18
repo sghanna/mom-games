798cd72989f04a3dc904f109234134eef32ead08
sghanna <sghanna@gmail.com>
2026-09-18 10:26:49 -0700

    Rebuild Hearts favicon with magenta ring and fix a broken hand-drawn A glyph

--- hearts/service-worker.js
+++ hearts/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "hearts-v15";
+const CACHE_NAME = "hearts-v16";
 const APP_FILES = [
   "./",
   "./index.html",
