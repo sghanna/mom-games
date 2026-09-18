22771c0a7b71e3f3543218eab7a1f20413e3f2bc
sghanna <sghanna@gmail.com>
2026-09-17 20:53:30 -0700

    Magenta selection highlight scaled to card width, +10% size on selected stack, matching favicon

--- hearts/service-worker.js
+++ hearts/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "hearts-v5";
+const CACHE_NAME = "hearts-v6";
 const APP_FILES = [
   "./",
   "./index.html",
