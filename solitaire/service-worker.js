e0a8428ea3ba90250dac80c1624d77efea6a051a
sghanna <sghanna@gmail.com>
2026-09-17 20:20:58 -0700

    New jumbo-card favicon, 20% rank-suit gap, bolder foundation markers, fruit confetti

--- solitaire/service-worker.js
+++ solitaire/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "solitaire-v3";
+const CACHE_NAME = "solitaire-v4";
 const APP_FILES = [
   "./",
   "./index.html",
