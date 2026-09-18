d68a14f8b29f79e2ebb1905c6e20a86825e5f7eb
sghanna <sghanna@gmail.com>
2026-09-17 22:53:48 -0700

    Revert clear-path preview celebration back to original colored confetti bars

--- solitaire/service-worker.js
+++ solitaire/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this cache version whenever a static file changes in a future update.
-const CACHE_NAME = "solitaire-v24";
+const CACHE_NAME = "solitaire-v25";
 const APP_FILES = [
   "./",
   "./index.html",
